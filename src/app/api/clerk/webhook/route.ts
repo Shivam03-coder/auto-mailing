3ab93204a26833a5e660a3d7167738092b16c40e
shivam anand <shivam850anand@gmail.com>
2024-12-06 20:52:28 +0530

    done

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -8,10 +8,8 @@ export const POST = async (req: Request) => {
   const imageUrl = data.image_url;
   const id = data.id;
 
-  await db.user.upsert({
-    where: { id },
-    update: { emailAddress, firstName, lastName, imageUrl },
-    create: { id, emailAddress, firstName, lastName, imageUrl },
+  await db.user.create({
+    data: { id, emailAddress, firstName, lastName, imageUrl },
   });
 
   return new Response("Webhook received", { status: 200 });
